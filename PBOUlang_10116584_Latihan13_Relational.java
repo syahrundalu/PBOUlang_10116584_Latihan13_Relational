599f4de244c93aaffa9f3c9abfcd56f33f32c7b6
Muhammad Syahrun Dalu <55743453+syahrundalu@users.noreply.github.com>
2019-09-26 13:57:22 +0700

    Add files via upload

---
+++ PBOUlang_10116584_Latihan13_Relational.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116584_Latihan13_Relational;
+
+/**
+ *
+ * @author 
+ * NIM      : 10116584
+ * NAMA     : Muhammad Syahrun Dalu
+ */
+public class PBOUlang_10116584_Latihan13_Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int bil1 = 10;
+       int bil2 = 20;
+        System.out.println("a == b = " +(bil1 == bil2));
+        System.out.println("a != b = " +(bil1 != bil2));
+        System.out.println("a > b  = " +(bil1 > bil2));
+        System.out.println("a < b  = " +(bil1 < bil2));
+        System.out.println("b >= a = " +(bil2 >= bil1));
+        System.out.println("b <= a = " +(bil2 <= bil1));
+    }
+    
+}
